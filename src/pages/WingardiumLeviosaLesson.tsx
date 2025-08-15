e410373ad94810bf050f5a2388511530ff2da651
ray-seal <rayseal1986@gmail.com>
2025-08-15 11:25:51 +0100

    Update WingardiumLeviosaLesson.tsx

--- src/pages/WingardiumLeviosaLesson.tsx
+++ src/pages/WingardiumLeviosaLesson.tsx
@@ -1,4 +1,4 @@
-import import React, { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Character } from "../types";
 import { houseThemes } from "../themes";
