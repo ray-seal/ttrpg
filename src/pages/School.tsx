e46ca54225f8066c245d5e11a73551b0d49b8dcd
ray-seal <rayseal1986@gmail.com>
2025-08-20 05:22:24 +0100

    Update School.tsx

--- src/pages/School.tsx
+++ src/pages/School.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Character } from "../types";
+import { Link } from "react-router-dom";
 
 const School: React.FC<{
   character: Character;
@@ -27,12 +28,59 @@ const School: React.FC<{
       <p>
         Welcome to Hogwarts, {character.name}! Your magical education begins here. Attend classes, meet professors, make friends, and discover secrets hidden within the castle's ancient walls.
       </p>
-      <ul style={{ marginTop: "1.5rem" }}>
-        <li>Explore the castle and grounds</li>
-        <li>Attend magical lessons</li>
-        <li>Complete quests and earn house points</li>
-        <li>Unlock new spells and abilities</li>
+
+      <h3 style={{ marginTop: "2rem", marginBottom: "0.7rem" }}>Key Locations</h3>
+      <ul>
+        <li>
+          <b>Owlery</b> <span style={{ color: "#b79b5a" }}>(coming soon)</span>
+        </li>
+        <li>
+          <b>Common Room</b> <span style={{ color: "#b79b5a" }}>(coming soon)</span>
+        </li>
+        <li>
+          <b>Great Hall</b> <span style={{ color: "#b79b5a" }}>(coming soon)</span>
+        </li>
       </ul>
+
+      <h3 style={{ marginTop: "2rem", marginBottom: "0.7rem" }}>Classes</h3>
+      <ul>
+        <li>
+          <b>Charms</b>
+          <ul style={{ marginTop: "0.5rem" }}>
+            <li>
+              <Link to="/charms/alohomora" style={{ color: "#4287f5", textDecoration: "none" }}>
+                Alohomora
+              </Link>
+            </li>
+            <li>
+              <Link to="/charms/wingardium-leviosa" style={{ color: "#4287f5", textDecoration: "none" }}>
+                Wingardium Leviosa
+              </Link>
+            </li>
+            <li>
+              <Link to="/charms/lumos" style={{ color: "#4287f5", textDecoration: "none" }}>
+                Lumos
+              </Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <b>Transfiguration</b> <span style={{ color: "#b79b5a" }}>(coming soon)</span>
+        </li>
+        <li>
+          <b>Potions</b> <span style={{ color: "#b79b5a" }}>(coming soon)</span>
+        </li>
+        <li>
+          <b>Defence Against the Dark Arts</b> <span style={{ color: "#b79b5a" }}>(coming soon)</span>
+        </li>
+        <li>
+          <b>Herbology</b> <span style={{ color: "#b79b5a" }}>(coming soon)</span>
+        </li>
+        <li>
+          <b>Care of Magical Creatures</b> <span style={{ color: "#b79b5a" }}>(coming soon)</span>
+        </li>
+      </ul>
+
       <div style={{ marginTop: "2rem", textAlign: "center" }}>
         <span role="img" aria-label="castle" style={{ fontSize: "2.5rem" }}>🏰</span>
       </div>
